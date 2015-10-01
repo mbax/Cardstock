f058255afe2516f4a7d335526c2c0924a9399fe4
Kyle Clemens <jkc.clemens@gmail.com>
2015-10-01 15:00:18 -0400

    Added: GameWithRounds

---
+++ src/main/kotlin/xyz/cardstock/cardstock/games/GameWithRounds.kt
@@ -0,0 +1,18 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+package xyz.cardstock.cardstock.games
+
+import org.kitteh.irc.client.library.element.Channel
+import xyz.cardstock.cardstock.Cardstock
+import xyz.cardstock.cardstock.games.rounds.Round
+import xyz.cardstock.cardstock.players.Player
+
+public abstract class GameWithRounds<PLAYER_TYPE : Player>(cardstock: Cardstock, channel: Channel, state: State) : Game<PLAYER_TYPE>(cardstock, channel, state) {
+
+    public var currentRound: Round<PLAYER_TYPE>? = null
+        protected set
+
+}
