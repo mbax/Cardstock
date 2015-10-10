b1da7699979d27530c40955f364079c421998a74
Kyle Clemens <jkc.clemens@gmail.com>
2015-10-09 21:22:06 -0400

    Fixed: Silly refactoring

--- src/main/kotlin/xyz/cardstock/cardstock/extensions/enums/Extensions.kt
+++ src/main/kotlin/xyz/cardstock/cardstock/extensions/enums/Extensions.kt
@@ -3,7 +3,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
-package xyz.cardstock.cardstock.extensions.enum
+package xyz.cardstock.cardstock.extensions.enums
 
 import xyz.cardstock.cardstock.interfaces.states.State
 
